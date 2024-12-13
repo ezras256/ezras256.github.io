dfec6c6210e909c0a7b35d333f10b4fee89d2fb0
ezras256 <120516359+ezras256@users.noreply.github.com>
2024-12-13 04:08:12 +0000

    Added main data functionality

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
-function openArtwork(PanelID) {
+function openArtwork(artistID, workID) {
     let data = JSON.parse(artistData);
-    // let title = 
+    let title = data["artists"]["" + artistID]["works"]["" + workID]["title"];
+    let created = data["artists"]["" + artistID]["works"]["" + workID]["created"];
+    let medium = data["artists"]["" + artistID]["works"]["" + workID]["medium"];
+    let contentPanel = document.getElementById("content-panel"); 
+    let contentText = document.getElementById("content-info"); 
+    let contentImage = document.getElementById("painting-image"); 
+    if (!contentPanelActive) {
+        contentPanel.classList.remove('selector-panel-inactive');
+        void contentPanel.offsetWidth; 
+        contentPanel.classList.add('selector-panel-active');
+    } 
+    // contentPanel.textContent = "Title: " + title + "\nMedium: " + medium + "\n Created on: " + created; 
+    contentText.childNodes[1].textContent = "Title: " + title;
+    contentText.childNodes[3].textContent = "Medium: " + medium;
+    contentText.childNodes[5].textContent = "Year Created: " + created;
+    console.log("url(/images/artworks/" + artistID + "/" + workID + ".jpg)"); 
+    contentImage.style.backgroundImage = "url(/images/artworks/" + artistID + "/" + workID + ".jpg)"; 
+}
+
+function changeContentPanel() {
+    let contentPanel = document.getElementById("content-panel"); 
+
+    
+
+    contentPanelActive = !contentPanelActive; 
+    firstArtwork = false; 
 }
 
 function activateSidePanel(PanelID) {
+    let artistPanel = document.getElementById("artist-panel"); 
+    
     let data = JSON.parse(artistData);
     let dataPath = data["artists"]["" + PanelID]["works"];
     let selectorPanel = document.getElementById('selector-panel');
     let childNodeArray = selectorPanel.childNodes;
     for (let i = 0; i < childNodeArray.length; i += 1) {
-        childNodeArray[i * 2 + 1].childNodes[0].textContent = dataPath["" + i]["title"];
+        if (i * 2 + 1 < 9) {
+            childNodeArray[i * 2 + 1].childNodes[0].textContent = dataPath["" + i]["title"];
+        }
     }
 
-    // localPanelID = PanelID; 
+    artistPanel.childNodes[1].textContent = data["artists"]["" + PanelID]["name"]; 
 
+}
+
+function setlocalID(id) {
+    localID = id;
 }
 
 function setContent() {
@@ -21,7 +54,10 @@ function setContent() {
 }
 
 function changeSideSelectorPanel(index) {
+    setlocalID(index);
+
     let selectorPanel = document.getElementById('selector-panel');
+    let contentPanel = document.getElementById('content-panel'); 
     let exitButton = document.getElementById('exit-button');
 
     void selectorPanel.offsetWidth;
@@ -33,7 +69,12 @@ function changeSideSelectorPanel(index) {
 
     activateSidePanel(index);
 
+
     void selectorPanel.offsetWidth;
+
+    contentPanel.classList.remove('selector-panel-active');
+    void contentPanel.offsetWidth; 
+    contentPanel.classList.add('selector-panel-inactive');
 }
 
 function toggleExitButton(state) {
@@ -64,4 +105,10 @@ function toggleSideSelectorPanel(state) {
         selectorPanel.classList.add('selector-panel-inactive');
         exitButton.classList.add('exit-button-inactive');
     }
+
+    if (contentPanelActive) {
+        contentPanel.classList.remove('selector-panel-active');
+        void contentPanel.offsetWidth; 
+        contentPanel.classList.add('selector-panel-inactive');
+    } 
 }
