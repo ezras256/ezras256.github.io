e7d06be4b37ab76ce43209e153b6ea90890d74aa
ezras256 <120516359+ezras256@users.noreply.github.com>
2024-12-13 01:58:13 +0000

    changed json to string and switched selector menu

--- index.js
+++ index.js
@@ -1,70 +1,67 @@
 function openArtwork(PanelID) {
-    
+    let data = JSON.parse(artistData);
+    // let title = 
 }
 
 function activateSidePanel(PanelID) {
+    let data = JSON.parse(artistData);
+    let dataPath = data["artists"]["" + PanelID]["works"];
+    let selectorPanel = document.getElementById('selector-panel');
+    let childNodeArray = selectorPanel.childNodes;
+    for (let i = 0; i < childNodeArray.length; i += 1) {
+        childNodeArray[i * 2 + 1].childNodes[0].textContent = dataPath["" + i]["title"];
+    }
+
+    // localPanelID = PanelID; 
 
 }
 
 function setContent() {
-    
-    let selectorPanel = document.getElementById('selector-panel'); 
-    
-
-}
-
-function getArtistData(name) {
-    fs.readFile('json/artworks_list.json', (err, inputD) => {
-        if (err) throw err;
-           alert(inputD.toString());
-     })
-
 
 }
 
 function changeSideSelectorPanel(index) {
-    let selectorPanel = document.getElementById('selector-panel'); 
-    // let oldSelectorPanel = document.getElementById('selector-panel-old'); 
-    let exitButton = document.getElementById('exit-button'); 
+    let selectorPanel = document.getElementById('selector-panel');
+    let exitButton = document.getElementById('exit-button');
 
-    void selectorPanel.offsetWidth; 
-    void exitButton.offsetWidth; 
+    void selectorPanel.offsetWidth;
+    void exitButton.offsetWidth;
     if (!sidePanelActive) {
-        toggleSideSelectorPanel(sidePanelActive); 
-        sidePanelActive = true; 
+        toggleSideSelectorPanel(sidePanelActive);
+        sidePanelActive = true;
     }
 
-    activateSidePanel(index); 
-    
-    void selectorPanel.offsetWidth; 
+    activateSidePanel(index);
+
+    void selectorPanel.offsetWidth;
 }
 
 function toggleExitButton(state) {
-    toggleSideSelectorPanel(state); 
-    sidePanelActive = false; 
+    toggleSideSelectorPanel(state);
+    sidePanelActive = false;
 }
 
 function toggleSideSelectorPanel(state) {
-    let selectorPanel = document.getElementById('selector-panel'); 
-    let exitButton = document.getElementById('exit-button'); 
-    
+    let selectorPanel = document.getElementById('selector-panel');
+    let exitButton = document.getElementById('exit-button');
+
     if (!state) {
-        selectorPanel.classList.remove('selector-panel-inactive'); 
-        exitButton.classList.remove('exit-button-inactive'); 
+        selectorPanel.classList.remove('selector-panel-inactive');
+        exitButton.classList.remove('exit-button-inactive');
 
         void selectorPanel.offsetWidth;
         void exitButton.offsetWidth;
 
-        selectorPanel.classList.add('selector-panel-active'); 
-        exitButton.classList.add('exit-button-active'); 
+        selectorPanel.classList.add('selector-panel-active');
+        exitButton.classList.add('exit-button-active');
     } else {
-        selectorPanel.classList.remove('selector-panel-active'); 
-        exitButton.classList.remove('exit-button-active'); 
-        
+        selectorPanel.classList.remove('selector-panel-active');
+        exitButton.classList.remove('exit-button-active');
+
         void selectorPanel.offsetWidth;
         void exitButton.offsetWidth;
 
-        selectorPanel.classList.add('selector-panel-inactive'); 
-        exitButton.classList.add('exit-button-inactive'); 
-    }    
+        selectorPanel.classList.add('selector-panel-inactive');
+        exitButton.classList.add('exit-button-inactive');
+    }
 }
